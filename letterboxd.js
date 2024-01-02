2efadb4d813ed72eef9f6e937d143b16169c68d1
Luvrok <pawel.2020.navtop@gmail.com>
2024-01-02 22:51:30 +0900

    initial

--- letterboxd.js
+++ letterboxd.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 const { parse } = require("node-html-parser");
 const axios = require("axios");
+require("dotenv").config();
 
 const OUTPUT_FILE = "result.json";
 
@@ -10,61 +11,16 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-class MessageQueue {
-  constructor() {
-    this.queue = [];
-    this.isProcessing = false;
-  }
-
-  async processQueue() {
-    if (this.isProcessing) return;
-    this.isProcessing = true;
-
-    while (this.queue.length) {
-      const { titlePush, messages } = this.queue.shift();
-
-      try {
-        let jsonData = [];
-        try {
-          const data = await fs.readFileSync(titlePush, "utf8");
-          jsonData = JSON.parse(data);
-        } catch (readError) {
-          if (readError.code !== "ENOENT") {
-            throw readError;
-          }
-        }
-
-        jsonData = jsonData.concat(messages);
-        await fs.writeFileSync(titlePush, JSON.stringify(jsonData));
-      } catch (err) {
-        console.error("Error processing message:", err);
-      }
-    }
-
-    this.isProcessing = false;
-  }
-
-  pushMessage(titlePush, messages) {
-    this.queue.push({ titlePush, messages });
-    this.processQueue();
-  }
-}
-
-async function pushMessage(titlePush, messages) {
-  const messageQueue = new MessageQueue();
-  messageQueue.pushMessage(titlePush, messages);
-}
-
 const USERNAME = process.argv[2];
 const URL = `https://letterboxd.com/${USERNAME}/films/page/`;
 
 async function getRenderedPage(url) {
-  const rendertronUrl = "http://167.99.72.39:8860/render"; // Замените на URL вашего экземпляра Rendertron
+  const rendertronUrl = `http://${process.env.URL_RENDERTRON}/render`;
   const targetUrl = encodeURIComponent(url);
 
   try {
     const response = await axios.get(`${rendertronUrl}/${targetUrl}`);
-    return response.data; // Возвращает отрендеренный HTML
+    return response.data;
   } catch (error) {
     console.error("Ошибка при запросе к Rendertron:", error);
     return null;
@@ -72,18 +28,7 @@ async function getRenderedPage(url) {
 }
 
 async function fetchPage(pageNumber) {
-  // const response = await fetch(URL + `${pageNumber}/`);
-  // const text = await response.text();
-  // console.log(text);
-
-  //http://167.99.72.39:8860
-
-  // const response = await axios.get(URL + `${pageNumber}/`);
-  // const text = response.data;
-
   const text = await getRenderedPage(URL + `${pageNumber}/`);
-  // console.log(text);
-
   return parse(text);
 }
 
@@ -96,37 +41,30 @@ async function getTotalPages(root) {
 }
 
 async function scrapeFilms() {
-  const root = await fetchPage(1);
-  const totalPages = await getTotalPages(root);
-  const films = [];
+  const root = await fetchPage(1),
+    totalPages = await getTotalPages(root),
+    films = [];
 
   for (let i = 1; i <= totalPages; i++) {
-    const pageRoot = await fetchPage(i);
-
-    console.log("page -- " + i);
-
-    const filmEntries = pageRoot.querySelectorAll(".poster-container");
+    const pageRoot = await fetchPage(i),
+      filmEntries = pageRoot.querySelectorAll(".poster-container");
 
     filmEntries.forEach((entry) => {
-      const $metadata = entry.querySelector("div");
+      const $metadata = entry.querySelector("div"),
+        filmTitle = $metadata.getAttribute("data-film-name");
 
-      const filmTitle = $metadata.getAttribute("data-film-name");
-      let year = $metadata.getAttribute("data-film-release-year");
+      let year = $metadata.getAttribute("data-film-release-year"),
+        element = entry.querySelector("span.rating.-tiny.-darker"),
+        rating = null;
 
       if (year) year = Number(year);
-
-      let element = entry.querySelector("span.rating.-tiny.-darker");
-
-      let rating = null;
       if (element) {
         let className = element.classNames;
-
         let matches = className.match(/rated-(\d+)/);
         rating = matches ? Number((Number(matches[1]) / 2).toFixed(1)) : null;
       }
 
       console.log({ filmTitle, year, rating });
-
       films.push({ filmTitle, year, rating });
     });
   }
@@ -148,5 +86,5 @@ scrapeFilms()
     console.log(`Total films: ${films.length}`);
   })
   .catch((error) => {
-    // console.error("Error scraping films:", error);
+    console.error("Error scraping films:", error);
   });
